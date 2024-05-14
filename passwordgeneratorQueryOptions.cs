b4d8777bdbf330ec966e468eb56caabc1c0c91c6
APIVerve Team <hello@apiverve.com>
2024-05-14 15:37:07 -0500

    Bug fixes and Improvements

---
+++ passwordgeneratorQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class passwordgeneratorQueryOptions {
+public int count { get; set; }
+public int length { get; set; }
+public string complexity { get; set; }
+}
+
+}
